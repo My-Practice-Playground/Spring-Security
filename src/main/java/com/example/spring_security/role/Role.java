c8031ad84a405a7ea7495bd4f716ee5b2aa0491b
Shan <perera.alc2000@gmail.com>
2023-09-27 15:57:00 +0530

    user roles added

---
+++ src/main/java/com/example/spring_security/role/Role.java
@@ -0,0 +1,5 @@
+package com.example.spring_security.role;
+
+public enum Role {
+    USER,ADMIN
+}
--- src/main/java/com/example/spring_security/user/User.java
+++ src/main/java/com/example/spring_security/user/User.java
@@ -1,11 +1,16 @@
 package com.example.spring_security.user;
 
 
+import com.example.spring_security.role.Role;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 @Data
 @NoArgsConstructor
@@ -13,7 +18,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @Entity
 @Table(name="_user")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
@@ -21,4 +26,36 @@ public class User {
     private String lastname;
     private String email;
     private String password;
+
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }
