8cb3fd29129f38a0cc56a32a9240f7e45ab13969
Shan <perera.alc2000@gmail.com>
2023-09-28 23:19:52 +0530

    JWT validator : done

--- src/main/java/com/example/spring_security/config/JWTService.java
+++ src/main/java/com/example/spring_security/config/JWTService.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 
 import java.security.Key;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
@@ -29,6 +30,10 @@ public class JWTService {
         return claimsResolver.apply(claims);
     }
 
+    public String generateToken(UserDetails userDetails){
+        return generateToken(new HashMap<>(),userDetails);
+    }
+
     public String generateToken(
             Map<String, Objects> extraClaims,
             UserDetails userDetails){
@@ -40,6 +45,22 @@ public class JWTService {
                 .setExpiration(new Date(System.currentTimeMillis()+1000*60*24))
                 .signWith(getSignInKey(), SignatureAlgorithm.ES256)
                 .compact();
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails){
+        final String username = extractUsername(token);
+        return (
+                username.equals(userDetails.getUsername())
+                        && !isTokenExpired(token)
+        );
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token,Claims::getExpiration);
     }
 
     private Claims extractAllClaims(String token) {
