d4987f398929af3ecce7ed69a5cc5a5c126f5c72
Shan <perera.alc2000@gmail.com>
2023-09-28 23:07:33 +0530

    extractClaim() added

--- src/main/java/com/example/spring_security/config/JWTService.java
+++ src/main/java/com/example/spring_security/config/JWTService.java
@@ -7,26 +7,32 @@ import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.function.Function;
 
 @Service
 public class JWTService {
 
-    private static final String SECURITY_KEY="6A2F8B4DCC103D82E371C7A9E7455E6F45A8D18C3D7E9A1B32529DEE7F67A6B9";
+    private static final String SECURITY_KEY = "6A2F8B4DCC103D82E371C7A9E7455E6F45A8D18C3D7E9A1B32529DEE7F67A6B9";
+
     String extractUsername(String token) {
         return null;
     }
 
-    private Claims extractAllClaims(String token){
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
         return Jwts
                 .parserBuilder()
                 .setSigningKey(getSignInKey())
                 .build()
-                .parseClaimsJws(token)
-                .getBody();
+                .parseClaimsJws(token).getBody();
     }
 
     private Key getSignInKey() {
-        byte[]keyBytes = Decoders.BASE64.decode(SECURITY_KEY);
+        byte[] keyBytes = Decoders.BASE64.decode(SECURITY_KEY);
         return Keys.hmacShaKeyFor(keyBytes);
     }
 }
