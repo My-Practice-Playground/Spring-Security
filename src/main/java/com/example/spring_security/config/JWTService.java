fc509a1332dcf0336ad49be4d669cab119e20be2
Shan <perera.alc2000@gmail.com>
2023-09-28 23:04:29 +0530

    claims method ready to use

--- src/main/java/com/example/spring_security/config/JWTService.java
+++ src/main/java/com/example/spring_security/config/JWTService.java
@@ -1,10 +1,32 @@
 package com.example.spring_security.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class JWTService {
+
+    private static final String SECURITY_KEY="6A2F8B4DCC103D82E371C7A9E7455E6F45A8D18C3D7E9A1B32529DEE7F67A6B9";
     String extractUsername(String token) {
         return null;
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[]keyBytes = Decoders.BASE64.decode(SECURITY_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
