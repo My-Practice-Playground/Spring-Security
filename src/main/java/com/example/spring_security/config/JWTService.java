592983a67e0aba26578366e32de78f47fc816e28
Shan <perera.alc2000@gmail.com>
2023-09-28 23:08:47 +0530

    extractUsername() implemented

--- src/main/java/com/example/spring_security/config/JWTService.java
+++ src/main/java/com/example/spring_security/config/JWTService.java
@@ -15,6 +15,7 @@ public class JWTService {
     private static final String SECURITY_KEY = "6A2F8B4DCC103D82E371C7A9E7455E6F45A8D18C3D7E9A1B32529DEE7F67A6B9";
 
     String extractUsername(String token) {
+        extractClaim(token,Claims::getSubject);
         return null;
     }
 
